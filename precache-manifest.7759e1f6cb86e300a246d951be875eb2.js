82314da68810591a6cff9ae50742420d6266fdeb
Nacho Caiafa <nachokai@outlook.com>
2020-05-21 15:18:36 -0300

    Updates

---
+++ precache-manifest.7759e1f6cb86e300a246d951be875eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f10b4cca8ba0afefc567e39921bd0f1",
+    "url": "/learning-redux/index.html"
+  },
+  {
+    "revision": "573e9fc8ab1ead8468f8",
+    "url": "/learning-redux/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08d1e0b40278f3b783ef",
+    "url": "/learning-redux/static/js/2.2cc1337d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learning-redux/static/js/2.2cc1337d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "573e9fc8ab1ead8468f8",
+    "url": "/learning-redux/static/js/main.179846e7.chunk.js"
+  },
+  {
+    "revision": "584e19a34a3573d937ee",
+    "url": "/learning-redux/static/js/runtime-main.28aa2dc5.js"
+  }
+]);

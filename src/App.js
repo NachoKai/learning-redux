cbf1706262b0bcd99b2aec9545e4232cea3e5f4a
Nacho Caiafa <nachokai@outlook.com>
2020-05-21 15:33:18 -0300

    add actions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './actions';
 
 function App() {
   const counter = useSelector(state => state.counter);
   const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
 
   return (
     <div className='App'>
       <h1>Hello Redux</h1>
       <h3>Counter {counter}</h3>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
       <h3>Secret Information:</h3>
       {isLogged ? (
         <h3>
